df57158a3f0944a763cd192fd88b272e86249886
Anna Lopatinski <lopatinski@gmail.com>
2018-03-27 13:24:16 +0300

    filter also apply all information

--- src/App.js
+++ src/App.js
@@ -118,6 +118,12 @@ class App extends Component {
     //     playlistElements.push(<Playlist playlist={playlist} />)
     //   }
     // }
+    let playlistsToRender = this.state.serverData.user ? this.state.serverData.user.playlists.filter(
+      playlist =>
+        playlist.name
+          .toLowerCase()
+          .includes(this.state.filterString.toLowerCase())
+    ): [];
     return (
       <div className="App">
         {this.state.serverData.user ? (
@@ -125,19 +131,15 @@ class App extends Component {
             <h1 style={{...defaultStyle, 'font-size': '54px'}}>
               {this.state.serverData.user.name}'s Playlists
             </h1>
-            <PlaylistCounter playlists={this.state.serverData.user.playlists} />
-            <HoursCounter playlists={this.state.serverData.user.playlists} />
+            <PlaylistCounter playlists={playlistsToRender} />
+            <HoursCounter playlists={playlistsToRender} />
             <Filter
               onTextChange={text => {
                 this.setState({filterString: text});
               }}
             />
             {/* {playlistElements} */}
-            {this.state.serverData.user.playlists
-              .filter(playlist =>
-                playlist.name.toLowerCase().includes(this.state.filterString)
-              )
-              .map(playlist => <Playlist playlist={playlist} />)}
+            {playlistsToRender.map(playlist => <Playlist playlist={playlist} />)}
           </div>
         ) : (
           <h1 style={defaultStyle}>Loading...</h1>
